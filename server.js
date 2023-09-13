de84977d6c51aa361d471d65f87c9ede9d8459d7
Alesia Kolbik <alesyakolbik@gmail.com>
2023-09-13 12:00:04 +0200

    routes

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    
+
+
+app.use('/users', (req, res, next) => {
+    console.log(`Hello!`)
+    res.send('<div><ul><li>user 1</li><li>user 2</li></ul></div>')
 })
+
+app.use('/', (req, res, next) => {
+    console.log(`Tt's working!`)
+    res.send('<div><h1>Hello from server!</h1></div>')
+})
+
+
+app.listen(3000);
